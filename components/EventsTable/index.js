6af26896d03f1532afcd3feb0d66ddaecd482a2f
Kristian <kristian@coulsy.co.uk>
2019-05-13 17:21:59 +0100

    Added Profile page

---
+++ components/EventsTable/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default () => {
+  return (
+    <table class="selector">
+      <thead>
+        <tr>
+          <td>Event</td>
+          <td>Date</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Molten Core</td>
+          <td>Wed 15th Nay</td>
+        </tr>
+        <tr>
+          <td>Molten Core</td>
+          <td>Wed 15th Nay</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
---
+++ components/UserProfileInfo/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default () => {
+  return (
+    <table class="info">
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Syth</td>
+        </tr>
+        <tr>
+          <td>Class</td>
+          <td>Warrior</td>
+        </tr>
+        <tr>
+          <td>Role</td>
+          <td>Tank</td>
+        </tr>
+        <tr>
+          <td>Rank</td>
+          <td>Guild Master</td>
+        </tr>
+        <tr>
+          <td>Profession</td>
+          <td>Mining</td>
+        </tr>
+        <tr>
+          <td>Profession</td>
+          <td>Engineering</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
