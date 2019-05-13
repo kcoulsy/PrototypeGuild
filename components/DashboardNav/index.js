f752ab91877aa94da8cbe6cb6d99c4bf617c9f23
Kristian <kristian@coulsy.co.uk>
2019-05-13 17:27:31 +0100

    Added edit profile page

--- components/DashboardNav/index.js
+++ components/DashboardNav/index.js
@@ -11,7 +11,7 @@ export default () => {
             <div className="nav-item"><Link href="/login"><a>Login</a></Link></div>
             <div className="nav-item"><Link href="/members"><a>Members</a></Link></div>
             <div className="nav-item"><Link href="/admin/applications"><a>applications</a></Link></div>
-            <div className="nav-item"><Link href="/admin/applicant"><a>applicant</a></Link></div>
+            <div className="nav-item"><Link href="/admin/applicant/"><a>applicant</a></Link></div>
             <div className="nav-item"><Link href="/admin/edit-profile"><a>edit profile</a></Link></div>
             <div className="nav-item"><Link href="/upcoming-events"><a>Events</a></Link></div>
             <div className="nav-item"><Link href="/profile"><a>My Profile</a></Link></div>
