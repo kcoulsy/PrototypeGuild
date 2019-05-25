fdb927320ad1c9a62e2209d02e2346899abddad1
Kristian <kristian@coulsy.co.uk>
2019-05-25 22:14:53 +0100

    Used carousel in index

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AuthService from '../utils/AuthService';
 import Navbar from '../components/Navbar';
 import Panel from '../components/Panel';
+import Carousel from '../components/Carousel';
 import Loader from '../components/Loader';
-
 
 const auth = new AuthService();
 
-export default () => {
-    return (
-        <div>
-            <Navbar auth={auth} />
-            <div className="content">
-                <Panel title="hello" className="">
-                    <Loader />
-                </Panel>
+export default class Index extends Component {
+    state = {
+        playerClasses: [],
+        featured: [],
+        posts: []
+    }
+    componentWillMount() {
+        auth.api('get', '/home').then(res => {
+            this.setState(res);
+        });
+    }
+    render() {
+        const {posts, playerClasses, featured} = this.state;
+        return (
+            <div>
+                <Navbar auth={auth} />
+                <div className="content">
+                    <Panel styleName="no-padding">
+                        <Carousel featured={featured}/>
+                        {/* <Loader /> */}
+                    </Panel>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
