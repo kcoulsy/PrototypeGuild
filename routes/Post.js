90b877bae9680f1964df0efe3dc5320702df32cf
Kristian <kristian@coulsy.co.uk>
2019-06-08 11:39:59 +0100

    Refactored post out into it's own file to tidy up routes

---
+++ routes/Post.js
@@ -0,0 +1,41 @@
+const { Post } = require('../Models/Post');
+
+exports.create = (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        body: req.body.body,
+        imageUrl: req.body.imageUrl,
+        createdAt: new Date(),
+        featured: req.body.featured
+    });
+    post.save().then(
+        doc => {
+            res.send(doc);
+        },
+        e => {
+            res.status(400).send(e);
+        }
+    );
+};
+
+exports.findById = (req, res) => {
+    Post.find({ _id: req.params.id }).then(
+        posts => {
+            res.send(posts);
+        },
+        e => {
+            res.status(400).send(e);
+        }
+    );
+};
+
+exports.find = (req, res) => {
+    Post.find().then(
+        posts => {
+            res.send(posts);
+        },
+        e => {
+            res.status(400).send(e);
+        }
+    );
+};
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-const { Post } = require('../Models/Post');
 const { Auth } = require('./../middleware/Auth');
 const { AdminAuth } = require('./../middleware/AdminAuth');
 
@@ -10,6 +9,7 @@ const User = require('./User');
 const Event = require('./Event');
 const Home = require('./Home');
 const Recruitment = require('./Recruitment');
+const Post = require('./Post');
 
 router.post('/users', User.create);
 router.post('/users/login', User.login);
@@ -32,46 +32,8 @@ router.get('/recruitment', Recruitment.find);
 router.post('/recruitment', AdminAuth, Recruitment.create);
 router.patch('/recruitment', AdminAuth, Recruitment.update);
 
-
-// router.post('/post', (req, res) => {
-//     const post = new Post({
-//         title: req.body.title,
-//         body: req.body.body,
-//         imageUrl: req.body.imageUrl,
-//         createdAt: new Date(),
-//         featured: req.body.featured
-//     });
-//     post.save().then(
-//         doc => {
-//             res.send(doc);
-//         },
-//         e => {
-//             res.status(400).send(e);
-//         }
-//     );
-// });
-
-// router.get('/posts/:id', (req, res) => {
-//     Post.find({ _id: req.params.id }).then(
-//         posts => {
-//             res.send(posts);
-//         },
-//         e => {
-//             res.status(400).send(e);
-//         }
-//     );
-// });
-
-// router.get('/posts', (req, res) => {
-//     Post.find().then(
-//         posts => {
-//             res.send(posts);
-//         },
-//         e => {
-//             res.status(400).send(e);
-//         }
-//     );
-// });
-
+router.post('/post', Post.create);
+router.get('/posts/:id', Post.findById);
+router.get('/posts', Post.find);
 
 module.exports = router;
