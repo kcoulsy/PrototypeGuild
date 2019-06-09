bd7285e3a6cee7a0b2dbf072edf13c0b3de0bbc5
Kristian <kristian@coulsy.co.uk>
2019-06-09 10:42:21 +0100

    Added edit more for profile pages for admins

--- pages/profile.js
+++ pages/profile.js
@@ -6,6 +6,9 @@ import Navbar from '../components/Navbar';
 import Panel from '../components/Panel';
 import Loader from '../components/Loader';
 
+import { CLASSES, ROLES, RANKS } from '../constants/users';
+import { PROFESSIONS } from '../constants/professions';
+
 class Profile extends Component {
     static async getInitialProps({ query }) {
         return { id: query.id };
@@ -13,7 +16,8 @@ class Profile extends Component {
 
     state = {
         user: {},
-        isLoading: true
+        isLoading: true,
+        editMode: false
     };
 
     componentDidMount() {
@@ -31,47 +35,211 @@ class Profile extends Component {
         });
     }
 
+    handleEditClick = () => {
+        const { auth } = this.props;
+        if (auth && auth.isAdmin) {
+            this.setState({ editMode: !this.state.editMode });
+        }
+    };
+
     render() {
         const { user, isLoading } = this.state;
         const { auth } = this.props;
+        const isAdmin = auth && auth.isAdmin();
+        const isEditMode = this.state.editMode && isAdmin;
 
         return (
             <div>
                 <Navbar auth={auth} />
                 <div className="content">
-                    <Panel title="Profile" styleName="panel-sm">
+                    <Panel title="Profile" styleName="panel-md no-padding">
                         {isLoading ? (
                             <Loader />
                         ) : (
-                            <table className="proto-table">
-                                <tbody>
-                                    <tr>
-                                        <td>Character Name</td>
-                                        <td>
-                                            {upperFirst(user.characterName)}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Class</td>
-                                        <td>{upperFirst(user.playerClass)}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Role</td>
-                                        <td>{upperFirst(user.playerRole)}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Rank</td>
-                                        <td>{upperFirst(user.rank)}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Professions</td>
-                                        <td>
-                                            {upperFirst(user.professionOne)}/
-                                            {upperFirst(user.professionTwo)}
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <form className="proto-form full-width">
+                                <table className="proto-table">
+                                    <tbody>
+                                        <tr>
+                                            <td>Character Name</td>
+                                            <td>
+                                                {isEditMode ? (
+                                                    <input
+                                                        className=""
+                                                        defaultValue={
+                                                            user.characterName
+                                                        }
+                                                    />
+                                                ) : (
+                                                    <span>
+                                                        {upperFirst(
+                                                            user.characterName
+                                                        )}
+                                                    </span>
+                                                )}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Class</td>
+                                            <td>
+                                                {isEditMode ? (
+                                                    <select
+                                                        defaultValue={user.playerClass.toLowerCase()}
+                                                    >
+                                                        {CLASSES.map(
+                                                            playerClass => {
+                                                                return (
+                                                                    <option
+                                                                        value={playerClass.toLowerCase()}
+                                                                    >
+                                                                        {
+                                                                            playerClass
+                                                                        }
+                                                                    </option>
+                                                                );
+                                                            }
+                                                        )}
+                                                    </select>
+                                                ) : (
+                                                    <span>
+                                                        {upperFirst(
+                                                            user.playerClass
+                                                        )}
+                                                    </span>
+                                                )}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Role</td>
+                                            <td>
+                                                {isEditMode ? (
+                                                    <select
+                                                        defaultValue={user.playerRole.toLowerCase()}
+                                                    >
+                                                        {ROLES.map(role => {
+                                                            return (
+                                                                <option
+                                                                    value={role.toLowerCase()}
+                                                                >
+                                                                    {role}
+                                                                </option>
+                                                            );
+                                                        })}
+                                                    </select>
+                                                ) : (
+                                                    <span>
+                                                        {upperFirst(
+                                                            user.playerRole
+                                                        )}
+                                                    </span>
+                                                )}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Rank</td>
+                                            <td>
+                                                {isEditMode ? (
+                                                    <select
+                                                        defaultValue={user.rank.toLowerCase()}
+                                                    >
+                                                        {RANKS.map(rank => {
+                                                            return (
+                                                                <option
+                                                                    value={rank.toLowerCase()}
+                                                                >
+                                                                    {rank}
+                                                                </option>
+                                                            );
+                                                        })}
+                                                    </select>
+                                                ) : (
+                                                    <span>
+                                                        {upperFirst(user.rank)}
+                                                    </span>
+                                                )}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Professions</td>
+                                            <td>
+                                                {isEditMode ? (
+                                                    <select
+                                                        defaultValue={user.professionOne.toLowerCase()}
+                                                    >
+                                                        {PROFESSIONS.map(
+                                                            prof => {
+                                                                return (
+                                                                    <option
+                                                                        value={
+                                                                            prof
+                                                                        }
+                                                                    >
+                                                                        {prof}
+                                                                    </option>
+                                                                );
+                                                            }
+                                                        )}
+                                                    </select>
+                                                ) : (
+                                                    <span>
+                                                        {upperFirst(
+                                                            user.professionOne
+                                                        )}
+                                                    </span>
+                                                )}
+                                                /
+                                                {isEditMode ? (
+                                                    <select
+                                                        defaultValue={user.professionTwo.toLowerCase()}
+                                                    >
+                                                        {PROFESSIONS.map(
+                                                            prof => {
+                                                                return (
+                                                                    <option
+                                                                        value={
+                                                                            prof.toLowerCase()
+                                                                        }
+                                                                    >
+                                                                        {prof}
+                                                                    </option>
+                                                                );
+                                                            }
+                                                        )}
+                                                    </select>
+                                                ) : (
+                                                    <span>
+                                                        {upperFirst(
+                                                            user.professionTwo
+                                                        )}
+                                                    </span>
+                                                )}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </form>
+                        )}
+                        {isAdmin && this.state.editMode ? (
+                            <div>
+                                <button
+                                    className="proto-btn"
+                                    onClick={this.handleEditClick}
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    className="proto-btn"
+                                    onClick={this.handleEditClick}
+                                >
+                                    Update
+                                </button>
+                            </div>
+                        ) : (
+                            <button
+                                className="proto-btn"
+                                onClick={this.handleEditClick}
+                            >
+                                Edit
+                            </button>
                         )}
                     </Panel>
                 </div>
