1c3520fe6d0def160b0b626c68e7fc8e77e0a4a7
Kristian <kristian@coulsy.co.uk>
2019-06-12 18:00:01 +0100

    Prettified _app and withAuth

--- utils/withAuth.js
+++ utils/withAuth.js
@@ -1,44 +1,44 @@
 // utils/withAuth.js - a HOC for protected pages
-import React, {Component} from 'react'
-import Router from 'next/router'
+import React, { Component } from 'react';
+import Router from 'next/router';
 import hoistNonReactStatics from 'hoist-non-react-statics';
-import AuthService from './AuthService'
+import AuthService from './AuthService';
 
 export default function withAuth(AuthComponent) {
-    const Auth = new AuthService()
-    
+    const Auth = new AuthService();
+
     class Authenticated extends Component {
-      static getInitialProps(ctx) {
-        return AuthComponent.getInitialProps(ctx)
-      }
-
-      constructor(props) {
-        super(props)
-        this.state = {
-          isLoading: true
-        };
-      }
-
-      componentDidMount () {
-        if (!Auth.loggedIn()) {
-            Router.push('http://localhost:3001/')
+        static getInitialProps(ctx) {
+            return AuthComponent.getInitialProps(ctx);
         }
-        this.setState({ isLoading: false })
-      }
 
-      render() {
-        const { isLoading } = this.state;
-        
-        return (
-          <div>
-          {isLoading ? (
-              <div>LOADING....</div>
-            ) : (
-              <AuthComponent {...this.props}  auth={Auth} />
-            )}
-          </div>
-        )
-      }
+        constructor(props) {
+            super(props);
+            this.state = {
+                isLoading: true
+            };
+        }
+
+        componentDidMount() {
+            if (!Auth.loggedIn()) {
+                Router.push(auth.domain);
+            }
+            this.setState({ isLoading: false });
+        }
+
+        render() {
+            const { isLoading } = this.state;
+
+            return (
+                <div>
+                    {isLoading ? (
+                        <div>LOADING....</div>
+                    ) : (
+                        <AuthComponent {...this.props} auth={Auth} />
+                    )}
+                </div>
+            );
+        }
     }
-    return hoistNonReactStatics(Authenticated,AuthComponent);
+    return hoistNonReactStatics(Authenticated, AuthComponent);
 }
