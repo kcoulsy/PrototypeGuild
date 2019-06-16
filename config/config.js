fbb43e3ad8c6c02e0c020502a302b616dd1ca037
Kristian <kristian@coulsy.co.uk>
2019-06-16 12:11:36 +0100

    Updated config and start script for prod

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = require('./config.json');
 const env = process.env.NODE_ENV || 'development';
 
 // Go through all keys in the config and set them in process.env
-if (env === 'development' || env === 'test') {
+if (env === 'development' || env === 'test' || env === 'production') {
     Object.keys(config[env]).forEach((key) => {
         process.env[key] = config[env][key];
     });
