0872af97a76d7e9415e5b860fdfbb6b35ee4059e
Kristian <kristian@coulsy.co.uk>
2019-06-16 11:13:17 +0100

    Linted NewRecruitment component

--- components/NewRecruitment.js
+++ components/NewRecruitment.js
@@ -1,41 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Link from 'next/link';
 
 import classes from '../constants/classes';
 
-export default class Recruitment extends Component {
-    render() {
-        const { classValues } = this.props;
-        return (
-            <div className="proto-recruitment-icons">
-                <h2>Recruitment</h2>
-                <div className="icons">
-                    {Object.keys(classes).map(playerClassKey => {
-                        const playerClass = classes[playerClassKey];
-                        const value = classValues.find(
-                            e => e.playerClass === playerClassKey.toLowerCase()
-                        );
-                        const recruiting = value && value.recruiting;
-                        return (
-                            <Link href="/apply" key={playerClassKey}>
-                                <div className="icon">
-                                    <img
-                                        src={`/static/images/classes/${
-                                            playerClass.name
-                                        }.png`}
-                                        alt={playerClass.name}
-                                    />
-                                    <span
-                                        className={`status ${
-                                            recruiting ? 'open' : 'closed'
-                                        }`}
-                                    />
-                                </div>
-                            </Link>
-                        );
-                    })}
-                </div>
+export default ({ classValues }) => {
+    return (
+        <div className="proto-recruitment-icons">
+            <h2>Recruitment</h2>
+            <div className="icons">
+                {Object.keys(classes).map(playerClassKey => {
+                    const playerClass = classes[playerClassKey];
+                    const value = classValues.find(
+                        e => e.playerClass === playerClassKey.toLowerCase()
+                    );
+                    const recruiting = value && value.recruiting;
+                    return (
+                        <Link href="/apply" key={playerClassKey}>
+                            <div className="icon">
+                                <img
+                                    src={`/static/images/classes/${playerClass.name}.png`}
+                                    alt={playerClass.name}
+                                />
+                                <span
+                                    className={`status ${
+                                        recruiting ? 'open' : 'closed'
+                                    }`}
+                                />
+                            </div>
+                        </Link>
+                    );
+                })}
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
