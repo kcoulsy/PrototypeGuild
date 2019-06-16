0ac801d5480290f8b99d5c6680baa662f2e239ca
Kristian <kristian@coulsy.co.uk>
2019-06-16 11:41:31 +0100

    Commented out carousel component

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,61 +1,61 @@
-import React, { Component } from "react";
-import Link from "next/link";
+// import React, { Component } from "react";
+// import Link from "next/link";
 
-class Carousel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      slides: props.featured.length,
-      currentCount: 1
-    };
-  }
+// class Carousel extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       slides: props.featured.length,
+//       currentCount: 1
+//     };
+//   }
 
-  componentDidMount() {
-    const intervalId = setInterval(this.timer, 6000);
-    // store intervalId in the state so it can be accessed later:
-    this.setState({ intervalId });
-  }
+//   componentDidMount() {
+//     const intervalId = setInterval(this.timer, 6000);
+//     // store intervalId in the state so it can be accessed later:
+//     this.setState({ intervalId });
+//   }
 
-  componentWillUnmount() {
-    // use intervalId from the state to clear the interval
-    clearInterval(this.state.intervalId);
-  }
+//   componentWillUnmount() {
+//     // use intervalId from the state to clear the interval
+//     clearInterval(this.state.intervalId);
+//   }
 
-  timer = () => {
-    const newCount = this.state.currentCount + 1;
-    if (newCount <= this.props.featured.length) {
-      this.setState({ currentCount: newCount });
-    } else {
-      this.setState({ currentCount: 1 });
-    }
-  };
+//   timer = () => {
+//     const newCount = this.state.currentCount + 1;
+//     if (newCount <= this.props.featured.length) {
+//       this.setState({ currentCount: newCount });
+//     } else {
+//       this.setState({ currentCount: 1 });
+//     }
+//   };
 
-  render() {
-    return (
-      <div className="proto-carousel">
-        {this.props.featured.map((item, index) => {
-          return (
-            <div
-              className={`item ${
-                index + 1 != this.state.currentCount ? "off-screen" : ""
-              }`}
-              key={index}
-              style={{ zIndex: index + 1 }}
-            >
-              <div className="overlay">
-                <Link href={`/post?id=${item._id}`} key={index}>
-                  <a>
-                    <h2>{item.title}</h2>
-                  </a>
-                </Link>
-                <p>{item.body}</p>
-              </div>
-              <img src={item.imageUrl} alt="" />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
-export default Carousel;
+//   render() {
+//     return (
+//       <div className="proto-carousel">
+//         {this.props.featured.map((item, index) => {
+//           return (
+//             <div
+//               className={`item ${
+//                 index + 1 != this.state.currentCount ? "off-screen" : ""
+//               }`}
+//               key={index}
+//               style={{ zIndex: index + 1 }}
+//             >
+//               <div className="overlay">
+//                 <Link href={`/post?id=${item._id}`} key={index}>
+//                   <a>
+//                     <h2>{item.title}</h2>
+//                   </a>
+//                 </Link>
+//                 <p>{item.body}</p>
+//               </div>
+//               <img src={item.imageUrl} alt="" />
+//             </div>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
+// export default Carousel;
