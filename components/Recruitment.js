438b8c5679efe90e7279168cae662e50a2b03ae2
Kristian <kristian@coulsy.co.uk>
2019-06-16 11:04:59 +0100

    Linted recruitment component

--- components/Recruitment.js
+++ components/Recruitment.js
@@ -1,35 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import classes from '../constants/classes';
 
-export default class Recruitment extends Component {
-    render() {
-        const {classValues} = this.props;
-        return (
-            <div className="proto-recruitment">
-                {/* <div className="title">Recruitment</div> */}
-                {Object.keys(classes).map(playerClassKey => {
-                    const playerClass = classes[playerClassKey];
-                    const value = classValues.find(
-                        e => e.playerClass === playerClassKey.toLowerCase()
-                    );
-                    const recruiting = value && value.recruiting;
-                    return (
-                        <div className="player-class" key={playerClassKey}>
-                            <img
-                                src={playerClass.image}
-                                alt={playerClass.name}
-                            />
-                            <span>{playerClass.name}</span>
-                            <span
-                                className={`status ${
-                                    recruiting ? 'open' : 'closed'
-                                }`}
-                            />
-                        </div>
-                    );x
-                })}
-            </div>
-        );
-    }
-}
+export default ({ classValues }) => {
+    return (
+        <div className="proto-recruitment">
+            {/* <div className="title">Recruitment</div> */}
+            {Object.keys(classes).map(playerClassKey => {
+                const playerClass = classes[playerClassKey];
+                const value = classValues.find(
+                    e => e.playerClass === playerClassKey.toLowerCase()
+                );
+                const recruiting = value && value.recruiting;
+                return (
+                    <div className="player-class" key={playerClassKey}>
+                        <img src={playerClass.image} alt={playerClass.name} />
+                        <span>{playerClass.name}</span>
+                        <span
+                            className={`status ${
+                                recruiting ? 'open' : 'closed'
+                            }`}
+                        />
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
