32d350f498571e0ca9304213bd4fb429bd28e451
Kristian <kristian@coulsy.co.uk>
2019-06-22 14:38:08 +0100

    Deleted site constant and package-lock from repo

--- constants/site.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    SITE_URL: 'http://127.0.0.1:3001/',
-    RECAPTCHA_KEY: '6LcOKaoUAAAAAPjSxKF5ZKj1q0QFmk7dqEVvJffw',
-    DISORD_EMBED_URL:
-        'https://discordapp.com/widget?id=531490243732570113&theme=dark'
-};
